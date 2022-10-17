796b56851369ef163506f85d43fcf7e8801fe25c
“yiping.wu” <yiping.wu@thoughtworks.com>
2022-10-17 16:16:46 +0800

    refactor: extract todolist and completed list component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import {useState} from "react";
-import { Checkbox } from 'antd';
-function TodoList({tasksList}) {
-    const todoTasks = tasksList.filter((task)=>
-        task.state === true
-    );
-
-    function onChange(e) {
-        console.log(`checked = ${e.target.checked}`);
-    }
-
-    return(
-        <>
-            {todoTasks.map((todo,index)=>
-                <Checkbox key={index} onChange={onChange}>{todo.name}</Checkbox>
-            )}
-        </>
-    )
-}
-function CompletedList({tasksList}) {
-    const todoTasks = tasksList.filter((task)=>
-        task.state === false
-    );
-
-    function onChange(e) {
-        console.log(`checked = ${e.target.checked}`);
-    }
-
-    return(
-        <>
-            {todoTasks.map((todo,index)=>
-                <Checkbox key={index} onChange={onChange} defaultChecked='true'>{todo.name}</Checkbox>
-            )}
-        </>
-    )
-}
+import {useEffect, useState} from "react";
+import {TodoList} from "./TodoList";
+import {CompletedList} from "./CompletedList";
 
 function App() {
-    const [tasksList] = useState([{name:'学英语',state:true},{name:'看电影',state:false}])
 
+    const [tasksList,setTaskList] = useState([{name:'学英语',state:true,id:1},{name:'看电影',state:false,id:2}])
+    const [todoTasks,setTodoTasks] = useState([])
+    const [completedTasks,setCompletedTasks] = useState([])
+    useEffect(()=>{
+        setTodoTasks(tasksList.filter((task)=>task.state === false));
+        setCompletedTasks(tasksList.filter((task)=>task.state === true));
+    },[tasksList])
+    const toggleTodoTask = (index,isChecked)=>{
+        todoTasks[index].state = isChecked;
+        setTodoTasks(todoTasks);
+        setTaskList([...todoTasks,...completedTasks]);
+        console.log('todo操作后');
+        console.log(tasksList);
+    }
+    const toggleCompletedTask = (index,isChecked)=>{
+        completedTasks[index].state = isChecked;
+        setCompletedTasks(completedTasks);
+        setTaskList([...todoTasks,...completedTasks]);
+        console.log('completed操作后')
+        console.log(tasksList);
+    }
   return (
     <div >
+        <input type="text"/>
       <h1>Todo List</h1>
-      <TodoList tasksList= {tasksList}/>
+      <TodoList todoTasks= {todoTasks} toggleTodoTask={toggleTodoTask}/>
       <h1>Completed List</h1>
-        <CompletedList tasksList= {tasksList} ></CompletedList>
+        <CompletedList completedTasks= {completedTasks} toggleCompletedTask={toggleCompletedTask}></CompletedList>
     </div>
   );
 }
