eb8e8e5d48e1ea5251848d8d66df5829f3362d83
camille.liu <camille.liu@proptrack.com>
2022-10-19 23:43:53 +0800

    [LK]refactor todo list

--- src/App.js
+++ src/App.js
@@ -3,62 +3,30 @@ import { TodoList } from './TodoList';
 import { CompletedList } from './CompletedList';
 import { Input } from 'antd';
 import 'antd/dist/antd.min.css';
+import { cloneDeep } from 'lodash';
 
 function App() {
-  const [tasksList, setTaskList] = useState([
-    { name: '学英语', state: true, readOnly: true },
-    { name: '看电影', state: false, readOnly: true }
-  ]);
+
+  const tasksList = [
+    { name: '学英语', completed: false, readOnly: true },
+    { name: '看电影', completed: true, readOnly: true }
+  ];
 
   const [todoTasks, setTodoTasks] = useState([]);
   const [completedTasks, setCompletedTasks] = useState([]);
   const [inputValue, setInputValue] = useState('');
 
-  const mapTask = { todo: todoTasks, completed: completedTasks };
-
-  useEffect(() => {
-    setTodoTasks(tasksList.filter((task) => task.state === false));
-    setCompletedTasks(tasksList.filter((task) => task.state === true));
-  }, [tasksList]);
-
-  const toggleTask = (e, index, flag) => {
-    const taskList = mapTask[flag];
-    const isChecked = e.target.checked;
-    taskList[index].state = isChecked;
-    if (flag === 'todo') {
-      setTodoTasks(taskList);
-    } else if (flag === 'completed') {
-      setCompletedTasks(taskList);
-    }
-    setTaskList([...todoTasks, ...completedTasks]);
-  };
-
-  const deleteTask = (index, flag) => {
-    const taskList = mapTask[flag];
-    taskList.splice(index, 1);
-    setTaskList([...todoTasks, ...completedTasks]);
-  };
-
   const addTask = (e) => {
-    const name = e.target.value;
-    const newTodoTask = { name, state: false, readOnly: true };
+    const newTodoTask = { name:e.target.value, completed: false, readOnly: true };
     setTaskList([...tasksList, newTodoTask]);
     setInputValue('');
   };
 
-  const editTask = (e, index, flag) => {
-    const taskName = e.target.value;
-    const taskList = mapTask[flag];
-    taskList[index].name = taskName;
-    taskList[index].readOnly = !taskList[index].readOnly;
-    setTaskList([...todoTasks, ...completedTasks]);
-  };
+  useEffect(()=>{
+    setTodoTasks(tasksList.filter((item)=>!item.completed));
+    setCompletedTasks(tasksList.filter((item)=>item.completed));
+  },[])
 
-  const editButton = (index, flag) => {
-    const taskList = mapTask[flag];
-    taskList[index].readOnly = false;
-    setTaskList([...todoTasks, ...completedTasks]);
-  };
 
   return (
     <div>
@@ -71,18 +39,16 @@ function App() {
       <h1>Todo List</h1>
       <TodoList
         todoTasks={todoTasks}
-        toggleTask={toggleTask}
-        deleteTask={deleteTask}
-        editTask={editTask}
-        editButton={editButton}
+        setTodoTasks={setTodoTasks}
+        completedTasks={completedTasks}
+        setCompletedTasks={setCompletedTasks}
       />
       <h1>Completed List</h1>
       <CompletedList
-        completedTasks={completedTasks}
-        toggleTask={toggleTask}
-        deleteTask={deleteTask}
-        editTask={editTask}
-        editButton={editButton}
+       todoTasks={todoTasks}
+       setTodoTasks={setTodoTasks}
+       completedTasks={completedTasks}
+       setCompletedTasks={setCompletedTasks}
       />
     </div>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,24 +1,51 @@
 import { Checkbox, Input } from 'antd';
+import { useEffect, useState } from 'react';
 import { Button } from 'antd';
+import { cloneDeep } from 'lodash';
 
-export function TodoList({ todoTasks, toggleTask, deleteTask, editTask, editButton }) {
-  const flag = 'todo';
+export function TodoList({ todoTasks,setTodoTasks,completedTasks,setCompletedTasks }) {
+
+  const[tempValue,setTempValue] = useState('');
+
+  const handleCheck = (e,index)=>{
+    setCompletedTasks([...completedTasks,todoTasks[index]]);
+    todoTasks.splice(index,1);
+  }
+
+  const onChange = (e,index)=>{
+    todoTasks[index].name=e.target.value
+    setTodoTasks(cloneDeep(todoTasks))
+  }
+
+  const handleEdit = (index)=>{
+    todoTasks[index].readOnly = !todoTasks[index].readOnly;
+    setTodoTasks(cloneDeep(todoTasks))
+  }
+
+  const handleDelete = (index)=>{
+    todoTasks.splice(index,1)
+    setTodoTasks(cloneDeep(todoTasks))
+  }
+
+
 
   return (
     <>
       {todoTasks.map((item, index) => {
         return (
           <div key={`${index}${item.name}`}>
-            <Checkbox onChange={(e) => toggleTask(e, index, flag)}>
+            <Checkbox onChange={(e) => handleCheck(e,index)}>
               <Input
-                defaultValue={item.name}
+                  defaultValue={item.name}
                 bordered={!item.readOnly}
                 readOnly={item.readOnly}
-                onPressEnter={(e) => editTask(e, index, flag)}
+                  autoFocus={!item.readOnly}
+                onChange={(e) => onChange(e,index)}
+                onPressEnter={(e) => handleEdit(index)}
               />
             </Checkbox>
-            <Button onClick={() => deleteTask(index, flag)}> delete</Button>
-            <Button onClick={() => editButton(index, flag)}> edit</Button>
+            <Button onClick={() => handleDelete(index)}> delete</Button>
+            <Button onClick={() => handleEdit(index)}> edit</Button>
           </div>
         );
       })}
